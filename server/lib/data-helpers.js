069cd735f580e393b1203b40e1d3162eb9798b96
esther-splett <esthersplett@gmail.com>
2019-03-21 21:08:24 +0000

    removed the hardcoded database and connected tweets to mongodb

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -7,22 +7,23 @@ const simulateDelay = require("./util/simulate-delay");
 // database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
-    // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
-    },
-
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      db.collection("tweets").find().sort().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
       });
+    },
+   saveTweet: function(newTweet, callback) {
+    db.collection('tweets').insertOne(newTweet, (err, r) =>{
+      if (err) {
+        return callback(err);
+      };
+      callback(null, newTweet);
+    });
     }
-
-  };
+  }
 }
+
