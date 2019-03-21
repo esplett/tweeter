fff934e6c3f9761fdae3f109cd88f4c2d280af5a
esther-splett <esthersplett@gmail.com>
2019-03-21 22:35:00 +0000

    fixed background image so it doesn't stretch with tweets and fixed syntax

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 
   $("textarea").on("keyup", function() {
     //sets the number on the counter
-    var counter = (140 - ($(this).val().length))
-    $("form").find(".counter").text(counter)
-    console.log(140 - ($(this).val().length))
+    var counter = (140 - ($(this).val().length));
+    $("form").find(".counter").text(counter);
+    console.log(140 - ($(this).val().length));
 
     if (counter < 0) {
-      $(".counter").addClass("red")
+      $(".counter").addClass("red");
     } else {
-      $(".counter").removeClass("red")
+      $(".counter").removeClass("red");
     }
   });
 });
