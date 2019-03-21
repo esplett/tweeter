4c3cb34ca8524637f5778a497f97b4a42b0b5872
esther-splett <esthersplett@gmail.com>
2019-03-21 22:37:33 +0000

    fixed js code adding semi-colons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -56,8 +56,8 @@ $(document).ready(function() {
   $( "form" ).on( "submit", function( event ) {
     event.preventDefault();
     var tweetText = $("textarea").val();
-    var errorBox = $(".error-message")
-    var errorText = $(".error-text")
+    var errorBox = $(".error-message");
+    var errorText = $(".error-text");
 
     if (tweetText === "") {
       errorText.text(function() {
